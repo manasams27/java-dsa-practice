a90c52405da8dc0ac8d59539a2605193227e1308
manasa <manasams2023@gmail.com>
2026-08-02 19:51:05 +0530

    Array problem solutions

---
+++ Arrays/AvgOfArrayElements.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class AvgOfArrayElements{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        int sum = 0;
+        int avg = 0;
+        for(int i=0; i<arr.length; i++){
+            sum += arr[i];
+            avg = sum / n;
+        }
+        System.out.println(avg);
+    }
+}
